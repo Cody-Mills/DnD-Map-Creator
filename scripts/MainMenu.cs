72f9d7f4deecccd32cb3f7cc956396075002a90c
Cody Mills <CodyMills11@hotmail.com>
2026-01-08 11:06:21 +0000

    Done Scripting Main Menu

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+
+public partial class MainMenu : CanvasLayer
+{
+    private Button _create;
+    private Button _load;
+    private Button _exit;
+
+    public override void _EnterTree()
+    {
+        _create = GetNode<Button>("Control/VBoxContainer/Create Map Button");
+        _load = GetNode<Button>("Control/VBoxContainer/Load Map Button");
+        _exit = GetNode<Button>("Control/VBoxContainer/Exit Button");
+
+        _create.Pressed += CreatePressed;
+        _load.Pressed += LoadPressed;
+        _exit.Pressed += ExitPressed;
+    }
+
+    private void CreatePressed()
+    {
+        //Load Creation UI
+        GD.Print("Create");
+    }
+
+    private void LoadPressed()
+    {
+        //Open File Explorer
+    }
+
+    private void ExitPressed()
+    {
+        GetTree().Quit();
+    }
+
+    public override void _ExitTree()
+    {
+        if (_create != null)
+        {
+            _create.Pressed -= CreatePressed;
+        }
+        if (_load != null)
+        {
+            _load.Pressed -= LoadPressed;
+        }
+        if (_exit != null)
+        {
+            _exit.Pressed -= ExitPressed;
+        }
+    }
+}
