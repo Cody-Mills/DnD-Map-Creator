8c96bee0ba1cbd5d1121dba821514ca0763e2e65
Cody Mills <CodyMills11@hotmail.com>
2026-01-08 18:48:38 +0000

    Removed alot of the stuff i got from a Tutorial and am going to do it my own way tommrow

--- scripts/EventManager.cs
+++ scripts/EventManager.cs
@@ -6,10 +6,7 @@ using Godot;
 public partial class EventManager : Node
 {
     public static Action<PackedScene> ItemSelectedEvent;
-    //Goes to EditorObject.cs & Sprite.cs
     public static void BroadcastItemSelectedEvent(PackedScene scene) { ItemSelectedEvent?.Invoke(scene);}
 
-    public static Action<Texture> SendTextureEvent;
-    //Goes to Item.cs
-    public static void BroadcastSendTextureEvent(Texture texture) { SendTextureEvent?.Invoke(texture);}
+
 }
